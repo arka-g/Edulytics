338db4fad876dd6f2ee2c63819571fd2dc808fb8
Arka Ganguli <arkaganguli1@gmail.com>
2014-12-22 19:49:48 -0500

    deleted markupold

--- server/hackru/public/Markup-old/app/app.js
+++
@@ -1,7 +0,0 @@
-// public/core.js
-var scotchTodo = angular.module('scotchTodo', []);
-
-scotchTodo.controller('mainController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
-  console.log("Test");
-}]);
