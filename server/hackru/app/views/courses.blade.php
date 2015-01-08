565b960e8cc6fb0b4f346b86c7b768637d74376e
Arka Ganguli <arkaganguli1@gmail.com>
2015-01-07 23:09:23 -0500

    new colour scheme

--- server/hackru/app/views/courses.blade.php
+++ server/hackru/app/views/courses.blade.php
@@ -8,8 +8,6 @@
       <title>School Hub</title>
       <!-- Bootstrap Core CSS -->
       <link href="/css/bootstrap.min.css" rel="stylesheet">
-      <!-- MetisMenu CSS -->
-      <link href="/css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
       <!-- Custom CSS -->
       <link href="/css/sb-admin-2.css" rel="stylesheet">
       <!-- Custom Fonts -->
